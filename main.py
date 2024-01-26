fc153e59cb45d424828fbc04bcd0ca61443a718d
Greeksouvlaki <basilisgim4@gmail.com>
2024-01-26 22:14:24 +0200

    Added saving password feature with valid checks

--- main.py
+++ main.py
@@ -1,8 +1,27 @@
 from tkinter import *
+from tkinter import messagebox
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+
+
+def save():
+    website = website_entry.get()
+    email = email_entry.get()
+    password = password_entry.get()
+
+    if len(website) == 0 or len(password) == 0:
+        messagebox.showinfo(title="Oops", message="Please don't leave any fields empty!")
+    else:
+        is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: \nEmail: {email} " f"\nPassword: {password} \nIs it ok to save?")
+
+        if is_ok:
+            with open("data.txt", mode="a") as file:  # "a" means append
+                file.write(f"{website} | {email} | {password}\n")
+                website_entry.delete(0, END)
+                password_entry.delete(0, END)
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -30,13 +49,14 @@ website_entry.grid(row=1, column=1, columnspan=2)
 website_entry.focus()  # cursor will be in this entry when the program starts
 email_entry = Entry(width=35)
 email_entry.grid(row=2, column=1, columnspan=2)
+email_entry.insert(0, "emailexample@youremail.com")  # default text
 password_entry = Entry(width=30)
 password_entry.grid(row=3, column=1)
 
 # Buttons
 generate_password_button = Button(text="Generate Password")
 generate_password_button.grid(row=3, column=2)
-add_button = Button(text="Add", width=36)
+add_button = Button(text="Add", width=36, command=save)
 add_button.grid(row=4, column=1, columnspan=2)
 
 
